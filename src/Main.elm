b58efb9530a303577de48dba581c685b016817f2
ponkotuy <web@ponkotuy.com>
2020-12-29 00:32:01 +0900

    Change Browser.application

--- src/Main.elm
+++ src/Main.elm
@@ -1,56 +1,83 @@
 module Main exposing (..)
 
 import Bootstrap.CDN as CDN
-import Bootstrap.Tab as Tab
-import Browser
-import Html exposing (Html, div, iframe, text)
+import Bootstrap.Tab as Tab exposing (Item, customInitialState)
+import Browser exposing (Document)
+import Browser.Navigation exposing (Key)
+import Html exposing (Html, iframe, text)
 import Html.Attributes exposing (attribute, src, style)
+import Url exposing (Url)
+import Url.Parser as Parser
+import Url.Parser.Query as Query
 
-main = Browser.sandbox { init = init, update = update, view = view }
+main = Browser.application
+  { init = init
+  , update = update
+  , view = view
+  , subscriptions = (\_ -> Sub.none)
+  , onUrlChange = UrlCahnge
+  , onUrlRequest = UrlRequest
+  }
 
-type Msg = SetTab Tab.State
+type Msg = SetTab Tab.State | UrlCahnge Url | UrlRequest Browser.UrlRequest
 
-type alias Model = { tab : Tab.State }
+type alias Model = { tab: Tab.State }
 
-init : Model
-init = { tab = Tab.initialState }
+type alias DofTab = { id: String, text: String, html: String }
 
-update : Msg -> Model -> Model
+tabs : List DofTab
+tabs =
+  [ {id = "from-lens", text = "距離と被写界深度", html = "from-lens.html"}
+  , {id = "f-overfocus", text = "F値と過焦点距離", html = "f-overfocus.html"}
+  , {id = "bokeh", text = "ボケ量", html = "bokeh.html"}
+  , {id = "bokeh-fvalue", text = "ボケとレンズスペック", html = "bokeh-need-fvalue.html"}
+  ]
+
+init : () -> Url -> Key -> (Model, Cmd msg)
+init _ url _ =
+  ({ tab = parseUrl url }, Cmd.none)
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-  case msg of
-    SetTab tab ->
-      { model | tab = tab }
+  let
+    newmodel =
+      case msg of
+        SetTab tab ->
+          { model | tab = tab }
+        UrlCahnge url ->
+          { model | tab = parseUrl url }
+        UrlRequest _ ->
+          model
+  in
+    (newmodel, Cmd.none)
 
 genIframe : String -> Html msg
 genIframe url =
   iframe [src url, attribute "width" "100%", attribute "height" "1080px", style "border" "0px"] []
 
-view : Model -> Html Msg
+viewItem : DofTab -> Item Msg
+viewItem tab =
+  Tab.item
+    { id = tab.id
+    , link = Tab.link [] [text tab.text]
+    , pane = Tab.pane [] [genIframe tab.html]
+    }
+
+view : Model -> Document Msg
 view model =
-  div []
-  [ CDN.stylesheet
-  , Tab.config SetTab
-    |> Tab.items
-      [ Tab.item
-        { id = "from-lens"
-        , link = Tab.link [] [text "距離と被写界深度"]
-        , pane = Tab.pane [] [genIframe "from-lens.html"]
-        }
-      , Tab.item
-        { id = "f-overfocus"
-        , link = Tab.link [] [text "F値と過焦点距離"]
-        , pane = Tab.pane [] [genIframe "f-overfocus.html"]
-        }
-      , Tab.item
-        { id = "bokeh"
-        , link = Tab.link [] [text "ボケ量"]
-        , pane = Tab.pane [] [genIframe "bokeh.html"]
-        }
-      , Tab.item
-        { id = "bokeh-fvalue"
-        , link = Tab.link [] [text "ボケとレンズスペック"]
-        , pane = Tab.pane [] [genIframe "bokeh-need-fvalue.html"]
-        }
-      ]
-    |> Tab.view model.tab
-  ]
+  { title = "被写界深度計算機"
+  , body =
+    [ CDN.stylesheet
+    , Tab.config SetTab
+      |> Tab.items (List.map viewItem tabs)
+      |> Tab.view model.tab
+    ]
+  }
+
+parseUrl : Url -> Tab.State
+parseUrl url =
+  let
+    parser = Query.string "tab"
+    id = Parser.parse parser url |> Maybe.withDefault "from-lens"
+  in
+    customInitialState id
